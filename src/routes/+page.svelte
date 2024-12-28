3d8ed795ecc9801001bcaf9c88ea1cd579a46557
Face <69168154+face-hh@users.noreply.github.com>
2024-12-28 20:19:11 +0200

    blinking logo & shadcn button on input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-	import { Input } from '$lib/components/ui/input';
+	import LogoBlink from '$lib/components/self/LogoBlink.svelte';
+	import { Button } from '$lib/components/ui/button';
+import { Input } from '$lib/components/ui/input';
 	import { Label } from '$lib/components/ui/label';
 
 	import { Search } from 'lucide-svelte';
@@ -13,19 +15,17 @@
 		>
 	</div>
 
-	<img src="/images/logo.svg" alt="Bliptext logo" class="w-48" />
+	<LogoBlink />
 
 	<!-- tricky way to get the icon button inside -->
-	<div class="w-full max-w-md px-4">
-		<div
-			class="relative flex [&:has(input:focus-visible)>button]:ring-1 [&:has(input:focus-visible)>button]:ring-ring"
-		>
-			<Input type="search" placeholder="Dogs..." class="w-full rounded-r-none" />
-			<button
-				class="flex items-center rounded-r-md border border-l-0 border-input bg-input px-3 transition-colors hover:bg-accent"
-			>
-				<Search class="h-4 w-4 text-muted-foreground" />
-			</button>
-		</div>
-	</div>
+    <div class="w-full max-w-md px-4">
+        <div
+            class="relative flex [&:has(input:focus-visible)>button]:ring-1 [&:has(input:focus-visible)>button]:ring-ring"
+        >
+            <Input type="search" placeholder="Dogs..." class="w-full rounded-r-none" />
+            <Button variant="outline" class="rounded-l-none border-l-0">
+                <Search class="h-4 w-4" />
+            </Button>
+        </div>
+    </div>
 </div>
