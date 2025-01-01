1ceb933e687b982d29cf49614aedcf83d27322a5
Face <69168154+face-hh@users.noreply.github.com>
2025-01-01 19:59:27 +0200

    move btns to separate component + tooltip

---
+++ src/lib/components/self/TooltipButton.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import * as Tooltip from '$lib/components/ui/tooltip';
+
+	let { icon, tooltipText, buttonClass = '', variant = 'outline' } = $props();
+</script>
+
+<Tooltip.Provider>
+	<Tooltip.Root>
+		<Tooltip.Trigger class={buttonClass}>
+			<Button {variant}>
+				<!-- svelte5 fuck sake -->
+				{@const Icon = icon}
+				<Icon class="h-4 w-4" />
+			</Button>
+		</Tooltip.Trigger>
+		<Tooltip.Content>
+			<p class="text-sm">{tooltipText}</p>
+		</Tooltip.Content>
+	</Tooltip.Root>
+</Tooltip.Provider>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import LogoBlink from '$lib/components/self/LogoBlink.svelte';
-	import { Button } from '$lib/components/ui/button';
-import { Input } from '$lib/components/ui/input';
+	import TooltipButton from '$lib/components/self/TooltipButton.svelte';
+    
+	import { Input } from '$lib/components/ui/input';
 	import { Label } from '$lib/components/ui/label';
-
-	import { Search } from 'lucide-svelte';
+	import { Search, Sparkles } from 'lucide-svelte';
 </script>
 
 <div class="flex h-screen flex-col items-center gap-4 pt-24">
@@ -17,15 +17,23 @@ import { Input } from '$lib/components/ui/input';
 
 	<LogoBlink />
 
-	<!-- tricky way to get the icon button inside -->
-    <div class="w-full max-w-md px-4">
-        <div
-            class="relative flex [&:has(input:focus-visible)>button]:ring-1 [&:has(input:focus-visible)>button]:ring-ring"
-        >
-            <Input type="search" placeholder="Dogs..." class="w-full rounded-r-none" />
-            <Button variant="outline" class="rounded-l-none border-l-0">
-                <Search class="h-4 w-4" />
-            </Button>
-        </div>
-    </div>
+	<div class="w-full max-w-md px-4">
+		<div class="flex gap-2">
+			<div
+				class="relative flex flex-1 [&:has(input:focus-visible)>button]:ring-1 [&:has(input:focus-visible)>button]:ring-ring"
+			>
+				<Input type="search" placeholder="Dogs..." class="w-full rounded-r-none" />
+
+				<TooltipButton
+					icon={Search}
+					tooltipText="Search for articles in the wiki"
+					buttonClass="rounded-l-none border-l-0"
+				/>
+			</div>
+			<TooltipButton
+				icon={Sparkles}
+				tooltipText="View random article"
+			/>
+		</div>
+	</div>
 </div>
