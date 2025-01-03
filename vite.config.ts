3d699d8c355dc134405b2052a8b85caf6d7e8324
Face <69168154+face-hh@users.noreply.github.com>
2025-01-03 11:23:51 +0200

    page optimization (1.3MB -> 251kb & 0.07s load)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'lucide-svelte/icons': new URL(
+				'./node_modules/lucide-svelte/dist/icons',
+				import.meta.url
+			).pathname
+		}
+	}
 });
