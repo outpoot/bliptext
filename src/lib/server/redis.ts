461e1388480a93b18b796a651d68ae64cc1a671a
Face <69168154+face-hh@users.noreply.github.com>
2025-01-19 10:50:07 +0200

    fix redis client config, was relying on sveltekit context

--- src/lib/server/redis.ts
+++ src/lib/server/redis.ts
@@ -1,7 +1,18 @@
 import Redis from 'ioredis';
-import { env } from '$env/dynamic/private';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const redis = new Redis(env.REDIS_URL!);
+try {
+    const { env } = await import('$env/dynamic/private');
+    process.env.REDIS_URL = env.REDIS_URL;
+} catch {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    dotenv.config({ path: path.join(__dirname, "../../../.env") });
+}
+
+const redis = new Redis(process.env.REDIS_URL!);
 
 redis.on('error', (err) => {
     console.error('Redis connection error:', err);
