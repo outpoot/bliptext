7033d6450b0e820752bf6f672c70ff2d8ec06c46
xyzeva <xyzeva@riseup.net>
2025-01-22 16:57:33 +0000

    convert to upstash redis for workers

--- src/lib/server/redis.ts
+++ src/lib/server/redis.ts
@@ -1,25 +1,21 @@
-import Redis from 'ioredis';
+import { Redis } from '@upstash/redis';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 try {
-    const { env } = await import('$env/dynamic/private');
-    process.env.REDIS_URL = env.REDIS_URL;
+	const { env } = await import('$env/dynamic/private');
+	process.env.REDIS_URL = env.REDIS_URL;
+	process.env.REDIS_TOKEN = env.REDIS_TOKEN;
 } catch {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-    dotenv.config({ path: path.join(__dirname, "../../../.env") });
+	const __filename = fileURLToPath(import.meta.url);
+	const __dirname = path.dirname(__filename);
+	dotenv.config({ path: path.join(__dirname, '../../../.env') });
 }
 
-const redis = new Redis(process.env.REDIS_URL!);
-
-redis.on('error', (err) => {
-    console.error('Redis connection error:', err);
-});
-
-redis.on('connect', () => {
-    console.log('Redis connected successfully');
+const redis = new Redis({
+	url: process.env.REDIS_URL,
+	token: process.env.REDIS_TOKEN
 });
 
 export { redis };
