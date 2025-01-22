8eead9d722f365767206ef6f0c29f6a767fbb61a
Face <69168154+face-hh@users.noreply.github.com>
2025-01-22 20:33:01 +0200

    (2) potential fix for cookies not being sent across subdomains

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -39,7 +39,24 @@ export const auth = betterAuth({
     advanced: {
         crossSubDomainCookies: {
             enabled: true,
-            domain: "bliptext.com"
+            domain: ".bliptext.com"
         },
+        useSecureCookies: true,
+        cookies: {
+            session_token: {
+                attributes: {
+                    sameSite: 'lax',
+                    path: '/',
+                    domain: '.bliptext.com'
+                }
+            },
+            session_data: {
+                attributes: {
+                    sameSite: 'lax',
+                    path: '/',
+                    domain: '.bliptext.com'
+                }
+            }
+        }
     }
 });
