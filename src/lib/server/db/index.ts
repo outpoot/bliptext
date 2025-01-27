8450e84fef4e5ffbdd6820a9b20ec728f743e1fd
xyzeva <xyzeva@riseup.net>
2025-01-27 18:15:35 +0000

    cache db conn

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -4,8 +4,17 @@ import { env } from '$env/dynamic/private';
 import * as schema from './schema';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+/**
+ * Cache the database connection in development. This avoids creating a new connection on every HMR
+ * update.
+ */
+const globalForDb = globalThis as unknown as {
+	client: postgres.Sql | undefined;
+};
 
-const client = postgres(env.DATABASE_URL);
+const client = globalForDb.client ?? postgres(env.DATABASE_URL);
+if (env.NODE_ENV !== 'production') globalForDb.client = client;
+
 export const db = drizzle(client, { schema });
 
 export const queries = {
