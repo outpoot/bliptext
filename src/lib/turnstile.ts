f12e1a49981cfcc6700b231bef21875c2296c36f
Face <69168154+face-hh@users.noreply.github.com>
2025-02-01 13:14:42 +0200

    use dynamic variables instead

--- src/lib/turnstile.ts
+++ src/lib/turnstile.ts
@@ -1,8 +1,8 @@
-import { TURNSTILE_SECRET_KEY } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
 export async function validateTurnstile(token: string): Promise<boolean> {
 	const formData = new FormData();
-	formData.append('secret', TURNSTILE_SECRET_KEY);
+	formData.append('secret', env.TURNSTILE_SECRET_KEY);
 	formData.append('response', token);
 
 	try {
