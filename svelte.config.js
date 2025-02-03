cd4892612f1187d0caa2f00ac623c4b3aa95ee42
Face <69168154+face-hh@users.noreply.github.com>
2025-02-03 20:22:58 +0200

    trying csrf disable alternative

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,16 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter(),
-		csrf: false
-	},
-
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter({
+            protocol_header: 'X-Forwarded-Proto',
+            host_header: 'X-Forwarded-Host',
+        }),
+        csrf: {
+            checkOrigin: true
+        }
+    }
 };
 
 export default config;
