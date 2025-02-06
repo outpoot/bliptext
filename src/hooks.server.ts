358799b2ce1c71aed6aaddb804fcf16d70393bd7
Face <69168154+face-hh@users.noreply.github.com>
2025-02-06 18:47:17 +0200

    dont cache auth middleware req

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,12 @@
-import { auth } from "$lib/auth"; // path to your auth file
+import { auth } from "$lib/auth";
 import { svelteKitHandler } from "better-auth/svelte-kit";
- 
+
 export async function handle({ event, resolve }) {
-    return svelteKitHandler({ event, resolve, auth });
+    const response = await svelteKitHandler({ event, resolve, auth });
+
+    response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+    response.headers.set('Pragma', 'no-cache');
+    response.headers.set('Expires', '0');
+
+    return response;
 }
