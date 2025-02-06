c0dbaf61ee75c7b5fafe1c2f3cec89bb8d3617c9
Face <69168154+face-hh@users.noreply.github.com>
2025-02-06 21:11:09 +0200

    nevermind we can just do this

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,5 +2,8 @@ import { auth } from "$lib/auth";
 import { svelteKitHandler } from "better-auth/svelte-kit";
 
 export async function handle({ event, resolve }) {
+    event.setHeaders({
+        'cache-control': 'private, no-cache, no-store, must-revalidate'
+    });
     return svelteKitHandler({ event, resolve, auth });
 }
