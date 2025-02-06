0550492a9da9c23b049110c7e4e74c4b22ac9e71
Face <69168154+face-hh@users.noreply.github.com>
2025-02-06 18:56:34 +0200

    fix empty asterisks allowance

--- src/lib/shared/wordMatching.ts
+++ src/lib/shared/wordMatching.ts
@@ -4,8 +4,8 @@ export function isValidWord(newWord: string): boolean {
     if (newWord.length > 100) return false;
 
     const isBoldOrItalic = (
-        /^\*\*[\s\w\d\p{P}]+\*\*$/u.test(newWord) ||
-        /^\*[\s\w\d\p{P}]+\*$/u.test(newWord)
+        /^\*\*[^\s*][\s\w\d\p{P}]*[^\s*]\*\*$/u.test(newWord) ||
+        /^\*[^\s*][\s\w\d\p{P}]*[^\s*]\*$/u.test(newWord)
     );
 
     const isLink = /^\[[\w\s\d\p{P}]+\]\([^\s]{1,100}\)$/u.test(newWord);
