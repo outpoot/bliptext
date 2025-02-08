973ee1310dc7de6b948e4bd8be3ceb396a8bdf03
Face <69168154+face-hh@users.noreply.github.com>
2025-02-08 12:11:23 +0200

    cache static media

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,8 +2,16 @@ import { auth } from "$lib/auth";
 import { svelteKitHandler } from "better-auth/svelte-kit";
 
 export async function handle({ event, resolve }) {
-    event.setHeaders({
-        'cache-control': 'private, no-cache, no-store, must-revalidate'
-    });
+    const path = event.url.pathname;
+    if (path.startsWith('/images/') || path.startsWith('/fonts/')) {
+        event.setHeaders({
+            'Cache-Control': 'public, max-age=31536000, immutable'
+        });
+    } else {
+        event.setHeaders({
+            'Cache-Control': 'private, no-cache, no-store, must-revalidate'
+        });
+    }
+
     return svelteKitHandler({ event, resolve, auth });
 }
