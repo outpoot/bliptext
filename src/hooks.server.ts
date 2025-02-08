4b1118b8bba7762abe6beb7ddaf84be5cf567a8a
Face <69168154+face-hh@users.noreply.github.com>
2025-02-08 12:21:02 +0200

    remove redundant caching

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,16 +2,9 @@ import { auth } from "$lib/auth";
 import { svelteKitHandler } from "better-auth/svelte-kit";
 
 export async function handle({ event, resolve }) {
-    const path = event.url.pathname;
-    if (path.startsWith('/images/') || path.startsWith('/fonts/')) {
-        event.setHeaders({
-            'Cache-Control': 'public, max-age=31536000, immutable'
-        });
-    } else {
-        event.setHeaders({
-            'Cache-Control': 'private, no-cache, no-store, must-revalidate'
-        });
-    }
+    event.setHeaders({
+        'Cache-Control': 'private, no-cache, no-store, must-revalidate'
+    });
 
     return svelteKitHandler({ event, resolve, auth });
 }
