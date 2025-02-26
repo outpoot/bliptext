74384d58325c755c1435dd664d1374a49b849016
Face <69168154+face-hh@users.noreply.github.com>
2025-02-26 11:01:54 +0200

    disallow zero width unicodes

--- src/lib/shared/wordMatching.ts
+++ src/lib/shared/wordMatching.ts
@@ -1,8 +1,10 @@
 export const WORD_MATCH_REGEX = /\*\*[^*]+\*\*|\*[^*]+\*|\[[^\]\*]+\]\([^\)]+\)|[^\s]+[.,_]?/g;
+const INVISIBLE_CHARS_REGEX = /[\u3164\u200B\u200C\u200D\uFEFF\u2060]/g;
 
 // warning - this function is not secure and should not be used for moderation
 export function isValidWord(newWord: string): boolean {
     if (newWord.length > 100) return false;
+    if (INVISIBLE_CHARS_REGEX.test(newWord)) return false;
 
     if (/[\uD800-\uDBFF][\uDC00-\uDFFF]/.test(newWord)) {
         return false;
