290bd3dacb066f0611760347750ff64c6194b942
Abdallah-Alwarawreh <Abooddevyt@outlook.com>
2025-03-01 21:11:17 +0300

    no linux mfs

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,18 @@
 import { auth } from "$lib/auth";
 import { svelteKitHandler } from "better-auth/svelte-kit";
+import { redirect } from "@sveltejs/kit";
 
 export async function handle({ event, resolve }) {
     event.setHeaders({
         'Cache-Control': 'private, no-cache, no-store, must-revalidate'
     });
+
+    // Stinky linux users don't deserve to be on the site ;)
+    const userAgent = event.request.headers.get('user-agent') || '';
+    console.log(userAgent);
+    if (userAgent.toLowerCase().includes('linux') && !event.url.pathname.includes('/api/')) {
+        throw redirect(301, 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    }
 
     return svelteKitHandler({ event, resolve, auth });
 }
